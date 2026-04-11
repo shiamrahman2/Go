090224ca182b53352e342e4354613a2431f2238e
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2026-04-11 17:11:29 +0600

    Package Scope

---
+++ add.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func add(x int, y int) {
+	z := x + y
+	fmt.Println(z)
+}
--- main.go
+++ main.go
@@ -1,31 +1,33 @@
 package main
+
 import "fmt"
-func practice(){
-  	// print 
+
+func practice() {
+	// print
 	fmt.Println("Hello, World!")
 	// variable declaration
-	var var_name int=10
+	var var_name int = 10
 	fmt.Println(var_name)
-	var a="Shiam Hosen Monna"
-	fmt.Println(a);
-	b:=true
-	fmt.Println(b);
+	var a = "Shiam Hosen Monna"
+	fmt.Println(a)
+	b := true
+	fmt.Println(b)
 	// reassign
-	b=false
-	fmt.Println(b);
-	 num := 0
+	b = false
+	fmt.Println(b)
+	num := 0
 
-    if num > 0 {
-        fmt.Println("Positive")
-    } else if num < 0 {
-        fmt.Println("Negative")
-    } else {
-        fmt.Println("Zero")
-    }
-	c:=4
-	switch c{
-	case 1: 
-	   fmt.Println("Number is one")
+	if num > 0 {
+		fmt.Println("Positive")
+	} else if num < 0 {
+		fmt.Println("Negative")
+	} else {
+		fmt.Println("Zero")
+	}
+	c := 4
+	switch c {
+	case 1:
+		fmt.Println("Number is one")
 	case 2:
 		fmt.Println("Number is Two")
 	case 3:
@@ -36,30 +38,32 @@ func practice(){
 		fmt.Println("Number Not Matched")
 	}
 }
-func input(){
+func input() {
 	fmt.Println("Welcome to our Application")
 	var name string
 	fmt.Println("Enter your name-")
 	fmt.Scanln(&name)
-	fmt.Println("Thanks ",name," to use our Application")
+	fmt.Println("Thanks ", name, " to use our Application")
 }
-func sum(num1 int,num2 int){
-	Sum:=num1+num2
+func sum(num1 int, num2 int) {
+	Sum := num1 + num2
 	fmt.Println(Sum)
 }
+
 // function with return type
-func add(num1 int,num2 int) int{
-	sum:=num1+num2
+func ad(num1 int, num2 int) int {
+	sum := num1 + num2
 	return sum
 }
+
 // function with multiple return value
-func getNumbers(num1 int,num2 int)(int,int){
-	sum:=num1+num2
-	mul:=num1*num2
-	return sum,mul
+func getNumbers(num1 int, num2 int) (int, int) {
+	sum := num1 + num2
+	mul := num1 * num2
+	return sum, mul
 }
-func main(){
-    // practice();
+func main() {
+	// practice();
 	// sum(10,20)
 	//  a:=30
 	//  b:=-3
@@ -68,5 +72,6 @@ func main(){
 	//  sum,mul:=getNumbers(a,b)
 	//  fmt.Println(sum);
 	//  fmt.Println(mul)
-	input()
+	//input()
+	add(5, 7)
 }
